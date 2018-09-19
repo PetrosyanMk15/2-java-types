ff345aeb09b0289b3ff888acfe8536ca76f9a072
Leva <petrosian.lyova@yandex.ru>
2018-09-19 10:45:58 +0500

    Петросян. Task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,9 +4,19 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int modX;
 
-        return "FALSE";
+        while (x  > 0){
+
+            modX = x % 10;
+
+            if (modX % 2 != 0)
+                return "FALSE";
+
+            x = x / 10;
+        }
+
+        return "TRUE";
     }
 
     public static void main(String[] args) {
