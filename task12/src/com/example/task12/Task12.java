f1090e0814b7afe340d4923eea0662a702910916
Leva <petrosian.lyova@yandex.ru>
2018-09-22 03:46:08 +0500

    Петросян. Task12

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -1,6 +1,7 @@
 package com.example.task12;
 
 import java.math.BigDecimal;
+import java.rmi.MarshalException;
 
 public class Task12 {
 
@@ -10,14 +11,10 @@ public class Task12 {
 
         // Считаем проценты за год
 
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+           sum = sum.add(sum.multiply(percent)).setScale(9, BigDecimal.ROUND_HALF_UP);
         }
         return sum;
-        */
-
-        return BigDecimal.ZERO;
     }
 
     public static void main(String[] args) {
